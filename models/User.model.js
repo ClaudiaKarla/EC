b318f2b43fee4cfb598a6a1625d989ca364c22db
ClaudiaKarla <115264070+ClaudiaKarla@users.noreply.github.com>
2023-02-12 09:58:21 -0600

    :seedling: Modelos definidos y Ruta de usuarios a mongo  :seedling:

--- models/User.model.js
+++ models/User.model.js
@@ -3,23 +3,38 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    nombre: {
       type: String,
-      required: false,
+      required: true
+    },
+    apellido: {
+      type: String,
+      required: true
+    },
+
+    nombreDeUsuario: {
+      type: String,
+      required: true,
       unique: true,
       trim: true,
     },
-    email: {
+    correo: {
       type: String,
       required: true,
       unique: true,
       trim: true,
       lowercase: true,
     },
-    password: {
+    contraseña: {
       type: String,
       required: true,
     },
+    telefono:String,
+    role:{
+      type:String,
+      enum:["cliente", "vendedor", "admin"],
+      default:"cliente"
+    }
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
