0f604ee3d318df50903e1aaff89e07f215cdf725
ClaudiaKarla <115264070+ClaudiaKarla@users.noreply.github.com>
2023-02-13 09:35:07 -0600

    Actualizacion el titulo del proyecto

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ require("./config")(app);
 const capitalize = require("./utils/capitalize");
 const projectName = "EC";
 
-app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
+app.locals.appTitle = `${capitalize(projectName)} `;
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
@@ -29,6 +29,12 @@ app.use("/", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
+
+
+////////////////////////////////////////////
+const adminRoutes = require("./routes/admin.routes");
+app.use("/admin", adminRoutes);
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
