10b322adcc21727863fc610275345b58b355fc2b
EmilianoCC <emiliano93cc@gmail.com>
2023-02-16 15:38:14 -0600

    Moment comentado

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv").config();
+require('dotenv').config()
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db')
 
-const moment =require("moment")
+// const moment =require("moment")
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express')
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
+const hbs = require('hbs')
 
 //formato fecha
-hbs.registerHelper("formatoFecha", (date)=>{
-    return moment(date).format("DD/MM/YYYY  HH:mn:ss")
-})
+// hbs.registerHelper("formatoFecha", (date)=>{
+//     return moment(date).format("DD/MM/YYYY  HH:mn:ss")
+// })
 
-const app = express();
+const app = express()
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
+require('./config')(app)
 
-const capitalize = require("./utils/capitalize");
-const projectName = "EC";
+const capitalize = require('./utils/capitalize')
+const projectName = 'EC'
 
-app.locals.appTitle = `${capitalize(projectName)} `;
+app.locals.appTitle = `${capitalize(projectName)} `
 
 // 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
-app.use("/", indexRoutes);
+const indexRoutes = require('./routes/index.routes')
+app.use('/', indexRoutes)
 
-const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
+const authRoutes = require('./routes/auth.routes')
+app.use('/auth', authRoutes)
 
-const productsRoutes = require("./routes/products.routes");
-app.use("/products", productsRoutes);
+const productsRoutes = require('./routes/products.routes')
+app.use('/products', productsRoutes)
 
-const sellerRoutes = require("./routes/seller.routes");
-app.use("/seller", sellerRoutes);
+const sellerRoutes = require('./routes/seller.routes')
+app.use('/seller', sellerRoutes)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app)
 
-module.exports = app;
+module.exports = app
